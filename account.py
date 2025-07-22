3ea223d86a99d2a3e235ea0a8ff9376de6177820
Laraib-Ansari <laraibansari1121@gmail.com>
2025-07-22 07:25:20 +0530

    Added Functions
    -> check balance
    -> deposit
    -> withdraw
    -> change password

--- account.py
+++ account.py
@@ -1,3 +1,36 @@
 class Account():
+    def __init__(self, name, password, balance):
+        self.name=name
+        self.password=password
+        self.balance
     
+    def chech_balance(self):
+        print(f"Your current balance is{self.balance:.2f}\n")
     
+    def deposit(Self, amount):
+        """
+        No negative amount shall be deposited
+
+        """
+        self.balance+= amount
+        print(f"INR {amount:.2f} deposited successfully\n")
+
+    def withdraw(Self, amount):
+        """
+        check if sufficient amount is present
+
+        """
+        if self.balance==0:
+            print("Balance is zero")
+
+        elif self.balance>=amount:
+            sefl.balance-=amount
+            print(f"INR {amount:.2f} has been withdrawn\n")
+        else:
+            print("Insufficient Balance")
+    
+    def change_password(self, new_password):
+        self.password=new_password
+        print("Password Updated\n")
+
+        
